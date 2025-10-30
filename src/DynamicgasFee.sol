ea6fa06f29c98eadf16ab784a5bfc99c9f4cc0d4
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-30 21:32:13 +0530

    added Hooks permissions

--- src/DynamicgasFee.sol
+++ src/DynamicgasFee.sol
@@ -9,3 +9,33 @@ import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
 import {SwapParams} from "v4-core/types/PoolOperation.sol";
+
+contract DynamicgasFee is BaseHook{
+        using LPFeeLibrary for uint24;
+
+        uint128 public movingAvarageGasPrice;
+        uint104 public movingAverageGasPriceCount;
+        constructor(IPoolManager) BaseHook(_manager){
+
+        }
+
+        function getHookPermissions() public pure override returns(Hooks.permissions.memory){
+              return
+            Hooks.Permissions({
+                beforeInitialize: true,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+        }
+}
