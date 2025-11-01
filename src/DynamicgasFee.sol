7664a8cd28b10a63030f1382c45fceaf7f0b7336
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-11-01 20:41:54 +0530

    added updateMovingAvarage()

--- src/DynamicgasFee.sol
+++ src/DynamicgasFee.sol
@@ -10,57 +10,65 @@ import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
 import {SwapParams} from "v4-core/types/PoolOperation.sol";
 
-contract DynamicgasFee is BaseHook{
-        using LPFeeLibrary for uint24;
+contract DynamicgasFee is BaseHook {
+    using LPFeeLibrary for uint24;
 
-        uint128 public movingAvarageGasPrice;
-        uint104 public movingAverageGasPriceCount;
-        uint24 public constant BASE_FEE = 5000;
+    uint128 public movingAvarageGasPrice;
+    uint104 public movingAverageGasPriceCount;
+    uint24 public constant BASE_FEE = 5000;
 
-        error MustHaveDynamicFee();
-        constructor(IPoolManager _poolManager) BaseHook(_poolManager){
-            updateMovingAverage();
-        }
+    error MustHaveDynamicFee();
 
-        function getHookPermissions() public pure override returns(Hooks.Permissions memory){
-              return
-            Hooks.Permissions({
-                beforeInitialize: true,
-                afterInitialize: false,
-                beforeAddLiquidity: false,
-                beforeRemoveLiquidity: false,
-                afterAddLiquidity: false,
-                afterRemoveLiquidity: false,
-                beforeSwap: true,
-                afterSwap: true,
-                beforeDonate: false,
-                afterDonate: false,
-                beforeSwapReturnDelta: false,
-                afterSwapReturnDelta: false,
-                afterAddLiquidityReturnDelta: false,
-                afterRemoveLiquidityReturnDelta: false
-            });
-        }
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {
+        updateMovingAverage();
+    }
 
-        function _beforeInitialize(address , PoolKey calldata key , uint160 sqrtPriceX96) internal pure override returns(bytes4){
-            if(!key.fee.isDynamic()) revert MustHaveDynamicFee();
-            return this.beforeInitialize.selector;
-        }
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: true,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterAddLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
 
-        function _beforeSwap(address,PoolKey calldata key , SwapParams , bytes) internal view override returns (bytes4, BeforeSwapDelta, uint24){
-            uint24 fee = getFee();
-        
+    function _beforeInitialize(address, PoolKey calldata key, uint160 sqrtPriceX96)
+        internal
+        pure
+        override
+        returns (bytes4)
+    {
+        if (!key.fee.isDynamic()) revert MustHaveDynamicFee();
+        return this.beforeInitialize.selector;
+    }
+
+    function _beforeSwap(address, PoolKey calldata key, SwapParams, bytes)
+        internal
+        view
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        uint24 fee = getFee();
+
         uint24 feeWithFlag = fee | LPFeeLibrary.OVERRIDE_FEE_FLAG;
-        return (
-            this.beforeSwap.selector,
-            BeforeSwapDeltaLibrary.ZERO_DELTA,
-            feeWithFlag
-        );
-        } 
+        return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, feeWithFlag);
+    }
 
-        function updateMovingAverage() internal {
-            uint128 gasPrice = uint128(tx.gasPrice);
+    function updateMovingAverage() internal {
+        uint128 gasPrice = uint128(tx.gasPrice);
 
-            movingAvarageGasPrice = ((movingAvarageGasPrice * movingAverageGasPriceCount) + gasPrice) / (movingAverageGasPriceCount + 1 );
-        }
+        movingAvarageGasPrice =
+            ((movingAvarageGasPrice * movingAverageGasPriceCount) + gasPrice) / (movingAverageGasPriceCount + 1);
+        movingAverageGasPriceCount++;
+    }
 }
